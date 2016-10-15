f633937c7e208012128fbd0537ed91635b66b048
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-10-15 13:55:13 +0100

    reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015101911) do
-
-
+ActiveRecord::Schema.define(version: 20161015094731) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -30,4 +28,5 @@ ActiveRecord::Schema.define(version: 20161015101911) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
