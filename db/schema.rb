382f75ba9a49993b889ae1c30c6cd6d1dabaf9be
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-10-15 13:47:54 +0100

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20161015101911) do
 
-  create_table "answers", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -37,13 +30,4 @@ ActiveRecord::Schema.define(version: 20161015101911) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
 
- # #2
-   describe "attributes" do
-     it "has title and body attributes" do
-       expect(post).to have_attributes(title: "New Post Title", body: "New Post Body")
-     end
-   end
+  let(:post) {Post.create!(title: "New Post Title", body: "New Post Body")}
+
+  describe "attributes" do
+    it "should respond to title" do
+      expect(post).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(post).to respond_to(:body)
+    end
+  end
+
 end
