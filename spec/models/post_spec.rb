7e9e3c1b2c2b3d760daac6f985580290eabbef6a
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-10-27 23:37:19 +0100

    partials and validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
  RSpec.describe Post, type: :model do
-  
+
    let(:name) { RandomData.random_sentence }
    let(:description) { RandomData.random_paragraph }
    let(:title) { RandomData.random_sentence }
@@ -12,6 +12,13 @@ require 'rails_helper'
    let(:post) { topic.posts.create!(title: title, body: body) }
 
    it { is_expected.to belong_to(:topic) }
+
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
    describe "attributes" do
      it "has a title and body attribute" do
