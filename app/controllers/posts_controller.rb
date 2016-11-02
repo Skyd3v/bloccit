08abbf1a40eadd8341e0f5c97cdffcc1806ce60b
Alex Anthopoulos <a.anthopoulos02@gmail.com>
2016-11-02 21:42:42 +0000

    add authorisation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     before_action :require_sign_in, except: :show
+    before_action :authorize_user, except: [:show, :new, :create]
 
     def show
         @post = Post.find(params[:id])
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
     end
 
     def create
-      @topic = Topic.find(params[:topic_id])
+        @topic = Topic.find(params[:topic_id])
         # #9
         @post = @topic.posts.build(post_params)
         @post.user = current_user
@@ -63,4 +64,13 @@ class PostsController < ApplicationController
     def post_params
         params.require(:post).permit(:title, :body)
     end
+
+    def authorize_user
+        post = Post.find(params[:id])
+        # #11
+        unless current_user == post.user || current_user.admin?
+            flash[:alert] = 'You must be an admin to do that.'
+            redirect_to [post.topic, post]
+        end
+   end
 end
